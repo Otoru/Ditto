d0f2dae62f71a2adb0bfbed1ead133a67b19ffb5
Vitor Hugo <vitoru@MacBook-Pro-de-Vitor.local>
2022-08-04 13:11:01 -0300

    :construction:  Start work to get logged user roles

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,6 +1,32 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0'
+import {
+  handleAuth,
+  handleLogin,
+  handleProfile,
+  AfterRefetch,
+} from '@auth0/nextjs-auth0'
+import { ManagementClient } from 'auth0'
 
 export default handleAuth({
+  async profile(req, res) {
+    try {
+      const refetch = true
+      const afterRefetch: AfterRefetch = (req, res, session) => {
+        const id = session.user.sub
+        const token = session.accessToken
+        const scope = process.env.AUTH0_SCOPE
+        const domain = process.env.AUTH0_ISSUER_BASE_URL!.replace(
+          'https://',
+          '',
+        )
+        const api = new ManagementClient({ token, domain, scope })
+        session.user.roles = api.getUserRoles({ id })
+        return session
+      }
+      await handleProfile(req, res, { refetch, afterRefetch })
+    } catch ({ message }) {
+      res.status(500).end(message)
+    }
+  },
   async login(req, res) {
     try {
       await handleLogin(req, res, {
@@ -10,7 +36,7 @@ export default handleAuth({
         },
       })
     } catch ({ message }) {
-      res.status(400).end(message)
+      res.status(500).end(message)
     }
   },
 })
